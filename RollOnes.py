9e8014c59e7d450bee3b3eef51949cbe76d32357
wickermoon <wickermoon@gmail.com>
2024-08-11 11:34:05 +0200

    save highest number, even if 177 is not hit

--- RollOnes.py
+++ RollOnes.py
@@ -3,7 +3,7 @@ import datetime
 import numpy
 from numpy.random import randint
 
-roll_sessions = 1000000000
+roll_sessions = 1000000
 result = 0
 
 d1 = datetime.datetime.now()
@@ -11,9 +11,9 @@ for _ in range(0, roll_sessions):
     rolled_array = randint(1, 5, size=231)
     numbers_of_rolled_ones = numpy.count_nonzero(rolled_array == 1)
 
-    if numbers_of_rolled_ones >= 177:
+    if result < numbers_of_rolled_ones:
         result = numbers_of_rolled_ones
-        break
+
 d2 = datetime.datetime.now()
 
 print(f"Highest Ones Roll: {result}")
