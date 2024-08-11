b1971d7df531b0581deb3f881aa2486a5db09d33
wickermoon <wickermoon@gmail.com>
2024-08-11 10:53:34 +0200

    add script

---
+++ RollOnes.py
@@ -0,0 +1,21 @@
+import datetime
+
+import numpy
+from numpy.random import randint
+
+roll_sessions = 1000000000
+result = 0
+
+d1 = datetime.datetime.now()
+for _ in range(0, roll_sessions):
+    rolled_array = randint(1, 5, size=231)
+    numbers_of_rolled_ones = numpy.count_nonzero(rolled_array == 1)
+
+    if numbers_of_rolled_ones >= 177:
+        result = numbers_of_rolled_ones
+        break
+d2 = datetime.datetime.now()
+
+print(f"Highest Ones Roll: {result}")
+print(f"Number of Roll Sessions: {roll_sessions}")
+print(f"Time taken: {d2 - d1}")
